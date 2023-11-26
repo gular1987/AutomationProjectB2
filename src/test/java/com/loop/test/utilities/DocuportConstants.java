a57798abba95331022dff69417a631cf7836a729
Gular <ghebibova@gmail.com>
2023-11-26 15:16:01 -0500

    My new commit day4

--- src/test/java/com/loop/test/utilities/DocuportConstants.java
+++ src/test/java/com/loop/test/utilities/DocuportConstants.java
@@ -7,6 +7,12 @@ public class DocuportConstants {
 
 
          public static final String LOGO_DOCUPORT = "Docuport";
+         public static final String PASSWORD_CLIENT="Group1";
+         public static  final  String RESET_PASSWORD_URL="reset_password";
+        public static final String RESET_PASSWORD_MESSAGE = "Enter the email address associated with your account";
+
+         public static final String EMAIL_FOR_RESET_PASSWORD = "forgotpasswordg1@gmail.com";
+
 
     }
 
